6c458d782c7709ace7719e8e8aa0ef3e1a2a61d8
Chris Alfano <chris@jarv.us>
2018-10-14 19:33:30 -0700

    feat: add habitat plan

---
+++ plan.sh
@@ -0,0 +1,34 @@
+pkg_name=php-core
+pkg_origin=emergence
+pkg_version="0.1.0"
+pkg_maintainer="Chris Alfano <chris@jarv.us>"
+pkg_license=("MIT")
+pkg_build_deps=(
+  core/composer
+)
+
+
+do_build() {
+  build_line "Copying core (excluding vendor)"
+  pushd "${PLAN_CONTEXT}" > /dev/null
+  find . \
+    -maxdepth 1 -mindepth 1 \
+    -not -name 'plan.sh' \
+    -not -name 'vendor' \
+    -not -name '.git*' \
+    -exec cp -r '{}' "${CACHE_PATH}/{}" \;
+  popd > /dev/null
+
+  build_line "Running: composer install"
+  pushd "${CACHE_PATH}" > /dev/null
+  COMPOSER_ALLOW_SUPERUSER=1 composer install --no-dev
+  popd > /dev/null
+}
+
+do_install() {
+  cp -r "${CACHE_PATH}"/* "${pkg_prefix}/"
+}
+
+do_strip() {
+  return 0
+}
