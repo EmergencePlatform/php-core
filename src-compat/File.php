fa3f6d5f2918ca1fbe50f252675046318f52c04e
Chris Alfano <chris@jarv.us>
2019-11-07 00:02:36 +0000

    feat: add more finfo methods to File class

--- src-compat/File.php
+++ src-compat/File.php
@@ -4,16 +4,9 @@ class File
 {
     public static $magicPath = null;// '/usr/share/misc/magic.mgc'
 
-    public static function getMIMEType($filename)
+    public static function getMIMEType($filePath)
     {
-        // get mime type
-        $finfo = finfo_open(FILEINFO_MIME, static::$magicPath);
-
-        if (!$finfo || !($mimeInfo = finfo_file($finfo, $filename))) {
-            throw new Exception('Unable to load file info');
-        }
-
-        finfo_close($finfo);
+        $mimeInfo = static::getFileInfo($filePath, FILEINFO_MIME);
 
         // split mime type
         $p = strpos($mimeInfo, ';');
@@ -23,18 +16,54 @@ class File
 
     public static function getMIMETypeFromContents($fileContents)
     {
-        // get mime type
-        $finfo = finfo_open(FILEINFO_MIME, static::$magicPath);
-
-        if (!$finfo || !($mimeInfo = finfo_buffer($finfo, $fileContents))) {
-            throw new Exception('Unable to load file info');
-        }
-
-        finfo_close($finfo);
+        $mimeInfo = static::getFileInfoFromContents($fileContents, FILEINFO_MIME);
 
         // split mime type
         $p = strpos($mimeInfo, ';');
 
         return $p ? substr($mimeInfo, 0, $p) : $mimeInfo;
+    }
+
+    public static function getExtension($filePath)
+    {
+        $extension = static::getFileInfo($filePath, FILEINFO_EXTENSION);
+
+        dd(compact('filePath', 'extension'));
+    }
+
+    public static function getExtensionFromContents($fileContents)
+    {
+        $extension = static::getFileInfoFromContent($fileContents, FILEINFO_EXTENSION);
+
+        dd(compact('extension'));
+        return $extension;
+    }
+
+    public static function getFileInfo($filePath, $options = FILEINFO_NONE)
+    {
+        // get mime type
+        $finfo = finfo_open($options, static::$magicPath);
+
+        if (!$finfo || !($fileInfo = finfo_file($finfo, $filePath))) {
+            throw new Exception('Unable to load file info');
+        }
+
+        finfo_close($finfo);
+
+        return $fileInfo;
+    }
+
+    public static function getFileInfoFromContents($fileContents, $options = FILEINFO_NONE)
+    {
+        // get mime type
+        $finfo = finfo_open(FILEINFO_MIME, static::$magicPath);
+
+        if (!$finfo || !($fileInfo = finfo_buffer($finfo, $fileContents))) {
+            throw new Exception('Unable to load file info');
+        }
+
+        finfo_close($finfo);
+
+        return $fileInfo;
     }
 }
