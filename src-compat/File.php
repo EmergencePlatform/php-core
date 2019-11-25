1bda8ce9b7c3ecfce559346a2db61ced6bbe242d
Chris Alfano <chris@jarv.us>
2019-11-25 22:48:46 +0000

    fix: harden Fileinfo magic path handling

--- src-compat/File.php
+++ src-compat/File.php
@@ -7,7 +7,7 @@ class File
     public static function getMIMEType($filename)
     {
         // get mime type
-        $finfo = finfo_open(FILEINFO_MIME, static::$magicPath);
+        $finfo = static::getFileInfoResource(FILEINFO_MIME);
 
         if (!$finfo || !($mimeInfo = finfo_file($finfo, $filename))) {
             throw new Exception('Unable to load file info');
@@ -24,7 +24,7 @@ class File
     public static function getMIMETypeFromContents($fileContents)
     {
         // get mime type
-        $finfo = finfo_open(FILEINFO_MIME, static::$magicPath);
+        $finfo = static::getFileInfoResource(FILEINFO_MIME);
 
         if (!$finfo || !($mimeInfo = finfo_buffer($finfo, $fileContents))) {
             throw new Exception('Unable to load file info');
@@ -36,5 +36,24 @@ class File
         $p = strpos($mimeInfo, ';');
 
         return $p ? substr($mimeInfo, 0, $p) : $mimeInfo;
+    }
+
+    protected static function getFileInfoResource($options = FILEINFO_NONE)
+    {
+        try {
+            // try with configured magicPath
+            $finfo = finfo_open($options, static::$magicPath);
+        } catch (Exception $e) {
+            try {
+                // try with environmental MAGIC if present
+                $finfo = finfo_open($options);
+            } catch (Exception $e) {
+                // try with environmental MAGIC unset
+                putenv('MAGIC');
+                $finfo = finfo_open($options);
+            }
+        }
+
+        return $finfo;
     }
 }
